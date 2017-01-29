3980d168641d8f2612a2466142d323adeaa9f5eb
Henrik Fricke <henrik.fricke@jimdo.com>
2017-01-29 19:00:51 +0100

    Handle requests to s3

--- functions/vellums.js
+++ functions/vellums.js
@@ -1,13 +1,20 @@
 'use strict';
 
-module.exports.get = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
+const AWS = require('aws-sdk');
+const s3 = new AWS.S3();
 
-  callback(null, response);
+module.exports.get = (event, context, callback) => {
+  var params = {
+    Bucket: process.env.BUCKET,
+    Key: event.path.user.toLowerCase() + '/' + event.path.vellum.toLowerCase() + '.md',
+  };
+  s3.getObject(params).promise()
+    .then(res => callback(null, {
+        Markdown: res.Body.toString('base64'),
+        LastModified: res.LastModified,
+        ContentLength: res.ContentLength,
+    }))
+    .catch(err => {
+      callback(new Error('[400] ' + err));
+    });
 };
